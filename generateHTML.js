9c2892fb75e9e7fd796a29ce4e2af6c496146baa
Chrisp1993 <chrispollio93@gmail.com>
2022-08-22 16:32:28 -0400

    added the arrays for the employee cards/roles

--- generateHTML.js
+++ generateHTML.js
@@ -35,3 +35,45 @@ const generateEngineer = function (engineer) {
     </div>
     `
 }
+
+// Generate Intern info
+const generateIntern = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+    </div>
+</div>
+    `
+};
+
+generateHTML = (data) => {
+    pageArray = []; 
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole(); 
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+            pageArray.push(managerCard);
+        }
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+            pageArray.push(engineerCard);
+        }
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+            pageArray.push(internCard);
+        }
+    }
+    const employeeCards = pageArray.join('')
+    const generateTeam = generateTeamPage(employeeCards); 
+    return generateTeam;
+}
